37081403074b70c12f28e370ddaf301b4abd6ee9
tenxdevelop <tenxdeveloper@bk.ru>
2025-07-19 18:29:46 +0500

    Added player service

---
+++ Assets/BonesOfDragonfall/Scripts/Game/Services/Gameplay/Player/IPlayerService.cs
@@ -0,0 +1,14 @@
+﻿/**************************************************************************\
+   Copyright SunWorldStudio Corporation. All Rights Reserved.
+\**************************************************************************/
+
+using UnityEngine;
+using System;
+
+namespace BonesOfDragonfall
+{
+    public interface IPlayerService : IDisposable
+    {
+        void Move(Vector2 direction);
+    }
+}
---
+++ Assets/BonesOfDragonfall/Scripts/Game/Services/Gameplay/Player/PlayerService.cs
@@ -0,0 +1,21 @@
+﻿/**************************************************************************\
+   Copyright SunWorldStudio Corporation. All Rights Reserved.
+\**************************************************************************/
+
+using UnityEngine;
+
+namespace BonesOfDragonfall
+{
+    public class PlayerService : IPlayerService
+    {
+        public void Dispose()
+        {
+            
+        }
+
+        public void Move(Vector2 direction)
+        {
+            
+        }
+    }
+}
