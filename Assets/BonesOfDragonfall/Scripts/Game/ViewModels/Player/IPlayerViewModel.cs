b7d6aeccd2d529f924099a640b98d761d04e0162
tenxdevelop <tenxdeveloper@bk.ru>
2025-07-19 18:26:11 +0500

    Added player view model and player view

---
+++ Assets/BonesOfDragonfall/Scripts/Game/ViewModels/Player/IPlayerViewModel.cs
@@ -0,0 +1,16 @@
+﻿/**************************************************************************\
+   Copyright SunWorldStudio Corporation. All Rights Reserved.
+\**************************************************************************/
+
+using SkyForge.Reactive;
+using SkyForge.MVVM;
+using UnityEngine;
+
+namespace BonesOfDragonfall
+{
+    public interface IPlayerViewModel : IViewModel
+    {
+        ReactiveProperty<Vector3> Position { get; }
+        ReactiveProperty<Quaternion> CameraRotation { get; }
+    }
+}
---
+++ Assets/BonesOfDragonfall/Scripts/Game/ViewModels/Player/PlayerViewModel.cs
@@ -0,0 +1,32 @@
+﻿/**************************************************************************\
+   Copyright SunWorldStudio Corporation. All Rights Reserved.
+\**************************************************************************/
+
+using SkyForge.Reactive;
+using UnityEngine;
+
+namespace BonesOfDragonfall
+{
+    public class PlayerViewModel : IPlayerViewModel
+    {
+        public ReactiveProperty<Vector3> Position { get; }
+        public ReactiveProperty<Quaternion> CameraRotation { get; }
+        
+        public void Dispose()
+        {
+            
+        }
+
+        public void Update(float deltaTime)
+        {
+            
+        }
+
+        public void PhysicsUpdate(float deltaTime)
+        {
+            
+        }
+
+       
+    }
+}
